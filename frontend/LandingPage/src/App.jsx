b7cbb5af752db7bb868f251dd4356ab335849e3e
Thales Plinio <thales_plinio@hotmail.com>
2025-04-28 16:13:14 -0400

    alterando as cores da página, colocando borda no nosso hero, e criando Tecnologies

--- frontend/LandingPage/src/App.jsx
+++ frontend/LandingPage/src/App.jsx
@@ -2,6 +2,7 @@ import { React } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Tecnology from './components/Tecnologies/Tecnology';
 import "./styles/Global.css";
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <ToastContainer autoClose={3000}/>
       <Header/>
       <Hero/>
+      <Tecnology/>
     </div>
   )
 }
---
+++ frontend/LandingPage/src/components/Tecnologies/Tecnology.jsx
@@ -0,0 +1,10 @@
+import styles from "./Tecnology.module.css";
+
+
+export default function Tecnology(){
+    return(
+        <section className={styles.tecContainer}>
+
+        </section>
+    )
+}
