5a392f08d2bacdac2f817fb07590c2b5b05beb60
Thales Plinio <thales_plinio@hotmail.com>
2025-04-28 12:10:25 -0400

    feito o header da aplicação com notificação no botão copiar, menu responsive, e foi adicionado também o wireframe da aplicação

--- frontend/LandingPage/src/App.jsx
+++ frontend/LandingPage/src/App.jsx
@@ -1,10 +1,15 @@
-import { React } from 'react'
+import { React } from 'react';
+import { ToastContainer } from 'react-toastify';
+import Header from './components/Header/Header';
+import "./styles/Global.css";
 
 function App() {
 
   return (
-    <>
-    </>
+    <div className="App">
+      <ToastContainer autoClose={3000}/>
+      <Header/>
+    </div>
   )
 }
 
---
+++ frontend/LandingPage/src/components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import { useRef, useState } from "react";
+import { IoMdClose } from "react-icons/io";
+import { IoMenu } from "react-icons/io5";
+import { toast } from "react-toastify";
+import styles from "./Header.module.css";
+
+export default function Header(){
+
+    const [showNav, setShowNav] = useState(false);
+    const emailRef = useRef(null);
+
+    const handleCopyEmail = () =>{
+        if (emailRef.current){
+            navigator.clipboard.writeText(emailRef.current.textContent)
+            .then(()=>{
+                toast.success("O E-Mail copiado...")
+            })
+        }
+    }
+
+    return (
+        <header className={styles.topHeader}>
+            <div className={styles.emailSection}>
+                <span ref={emailRef} className={styles.emailExemple}>thalesfeitosa.dev@gmail.com</span>
+                <button className={styles.btnCopy} onClick={handleCopyEmail}>Copiar</button>
+            </div>
+
+            <div className={styles.menuToggle} onClick={() => setShowNav(!showNav)}>
+                {showNav ? <IoMdClose /> : <IoMenu /> }
+            </div>            
+
+            <nav className={`${styles.socialMidiaLinks} ${showNav ? styles.show : ''}`}>
+                <a href="#">Linkedin</a>
+                <a href="#">Github</a>
+                <a href="#">Instagran</a>
+            </nav>
+        </header>
+    );
+}
