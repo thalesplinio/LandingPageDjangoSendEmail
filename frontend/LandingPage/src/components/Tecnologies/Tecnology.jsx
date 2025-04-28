290b77fe268ab9800658d8405842c61b14ff9704
Thales Plinio <thales_plinio@hotmail.com>
2025-04-28 16:35:35 -0400

    criado as tecnologias, com um carrossel infinito aparecendo as tecnologias que tem conhecimento.

--- frontend/LandingPage/src/components/Tecnologies/Tecnology.jsx
+++ frontend/LandingPage/src/components/Tecnologies/Tecnology.jsx
@@ -2,9 +2,27 @@ import styles from "./Tecnology.module.css";
 
 
 export default function Tecnology(){
+
+    const tecnologies = [
+        "JavaScript",
+        "Python",
+        "Django",
+        "C# .NET",
+        "Java",
+        "Spring",
+        "MySQL",
+        "PostgreSQL"
+    ]
+
     return(
         <section className={styles.tecContainer}>
+            <div className={styles.slider}>
 
+                {tecnologies.concat(tecnologies).map((tech, index) => (
+                    <span key={index}>{tech}</span>
+                ))}
+
+            </div>
         </section>
     )
 }
