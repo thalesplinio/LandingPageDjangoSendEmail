a9a082f180f4e3595ddb69086fbdb43f06db0af8
Thales Plinio <thales_plinio@hotmail.com>
2025-04-29 18:00:05 -0400

    construção service, onde vai conter portfólio

--- frontend/LandingPage/src/App.jsx
+++ frontend/LandingPage/src/App.jsx
@@ -2,6 +2,7 @@ import { React } from 'react';
 import { ToastContainer } from 'react-toastify';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
+import Services from './components/Services/Services';
 import Tecnology from './components/Tecnologies/Tecnology';
 import "./styles/Global.css";
 
@@ -13,6 +14,7 @@ function App() {
       <Header/>
       <Hero/>
       <Tecnology/>
+      <Services/>
     </div>
   )
 }
---
+++ frontend/LandingPage/src/components/Services/Services.jsx
@@ -0,0 +1,36 @@
+import img from "../../assets/img/computer.png";
+import Button from "../Buttons/Button";
+import styles from "./Services.module.css";
+
+
+export default function Services(){
+    return(
+        <section className={styles.services}>
+            <h2>Explore meu portifólio de projetos</h2>
+            <div className={styles.line}></div>
+            <div className={styles.containerService}>
+                <div className={styles.serviceCards}>
+                    <img src={img} alt="" />
+                    <h3>Project Title 1</h3>
+                    <span>Django, ReactJS, MySQL</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
+                    <Button content="Ver mais"></Button>
+                </div>
+                <div className={styles.serviceCards}>
+                    <img src={img} alt="" />
+                    <h3>Project Title 1</h3>
+                    <span>Django, ReactJS, MySQL</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
+                    <Button content="Ver mais"></Button>
+                </div>
+                <div className={styles.serviceCards}>
+                    <img src={img} alt="" />
+                    <h3>Project Title 1</h3>
+                    <span>Django, ReactJS, MySQL</span>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
+                    <Button content="Ver mais"></Button>
+                </div>
+            </div>
+        </section>
+    )
+}
