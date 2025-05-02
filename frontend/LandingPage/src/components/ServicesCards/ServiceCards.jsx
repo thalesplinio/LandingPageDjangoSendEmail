d9dbc01825f87051e8b87b6ccde2bb21680cac47
Thales Plinio <thales_plinio@hotmail.com>
2025-05-01 20:22:28 -0400

    feita a confituração do front-end ServiceCards para receber os dados da api

--- frontend/LandingPage/src/components/ServicesCards/ServiceCards.jsx
+++ frontend/LandingPage/src/components/ServicesCards/ServiceCards.jsx
@@ -1,35 +1,38 @@
-import img from "../../assets/img/computer.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 import Button from "../Buttons/Button";
 import styles from "./ServiceCards.module.css";
 
 
 export default function ServiceCards(){
+    const [ project, setProject ] = useState([]);
+    const endpointProject = "http://127.0.0.1:8000/api/projects/";
+
+    useEffect(() => {
+        axios.get(endpointProject)
+        .then(response => setProject(response.data))
+        .catch(error =>{ console.log("Tivemos um erro ao carregar ----- ", error)})
+    }, [])
+
+    if (!project) return <p>Carregando...</p>
+
+
     return(
         <section className={styles.services}>
             <h2>Explore meu portifólio de projetos</h2>
             <div className={styles.line}></div>
             <div className={styles.containerService}>
-                <div className={styles.serviceCards}>
-                    <img src={img} alt="" />
-                    <h3>Project Title 1</h3>
-                    <span>Django, ReactJS, MySQL</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
-                    <Button content="Ver mais"></Button>
-                </div>
-                <div className={styles.serviceCards}>
-                    <img src={img} alt="" />
-                    <h3>Project Title 1</h3>
-                    <span>Django, ReactJS, MySQL</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
-                    <Button content="Ver mais"></Button>
-                </div>
-                <div className={styles.serviceCards}>
-                    <img src={img} alt="" />
-                    <h3>Project Title 1</h3>
-                    <span>Django, ReactJS, MySQL</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod, adipisci.</p>
-                    <Button content="Ver mais"></Button>
-                </div>
+                {
+                    project.map((item) => (
+                        <div className={styles.serviceCards}>
+                            <img src={item.icon} alt="" />
+                            <h3>{item.title}</h3>
+                            <span>{item.subtitle}</span>
+                            <p>{item.description}</p>
+                            <Button content="Ver mais"></Button>
+                        </div>
+                    ))
+                }
             </div>
         </section>
     )
