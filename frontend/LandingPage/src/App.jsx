01d6632bb3013efc1b321a0b1450a11c339f1a83
Thales Plinio <thales_plinio@hotmail.com>
2025-05-28 17:29:48 -0400

    adicionando titulo no no site

--- frontend/LandingPage/src/App.jsx
+++ frontend/LandingPage/src/App.jsx
@@ -1,4 +1,6 @@
-import { React } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
+import { SkeletonTheme } from 'react-loading-skeleton';
 import { ToastContainer } from 'react-toastify';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
@@ -6,10 +8,21 @@ import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import ServiceCards from './components/ServicesCards/ServiceCards';
 import Tecnology from './components/Tecnologies/Tecnology';
+import { API_ENDPOINTS } from "./services/api";
 import "./styles/Global.css";
-import Skeleton, { SkeletonTheme } from 'react-loading-skeleton';
+
 
 function App() {
+
+  useEffect(() => {
+    axios.get(API_ENDPOINTS.SITE_SETUP)
+      .then(res => {
+        const siteName = res.data[0].name;
+        document.title = `|| ${siteName}`
+      })
+      .catch(() => { document.title = "Fora do ar | Offline"})
+  }, []);
+
 
   return (
     <div className="App">
