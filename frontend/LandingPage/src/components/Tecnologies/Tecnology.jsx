a2af1cbab0ff292e9c487ca41fc21e78455e3b4c
Thales Plinio <thales_plinio@hotmail.com>
2025-05-28 11:35:08 -0400

    adicionando variavel de ambiente no Technology.jsx

--- frontend/LandingPage/src/components/Tecnologies/Tecnology.jsx
+++ frontend/LandingPage/src/components/Tecnologies/Tecnology.jsx
@@ -6,28 +6,30 @@ import styles from "./Tecnology.module.css";
 export default function Tecnology(){
 
     const [ tech, setTech ] = useState([]);
-    const endpointTech = "http://127.0.0.1:8000/api/technology/";
+    const API_BASE = import.meta.env.VITE_REACT_APP_API_URL;
+    const pointTech = API_BASE + "/api/technology/";
+
     useEffect(() => {
-        axios.get(endpointTech)
+        axios.get(pointTech)
         .then(response => setTech(response.data))
         .catch(error => { console.log("Tivemos um erro ao carregar ----- ", error)})
     }, [])
-    if (!tech) return <p>Carregando...</p>
+    
+    if (tech.length === 0) return <p>Carregando...</p>
 
     return(
         <section className={styles.tecContainer}>
             <div className={styles.slider}>
                 {
-                    tech.map((item) => (
+                    tech.map((item) =>(
                         <span key={item.id}>{item.name}</span>
                     ))
                 }
                 {
-                    tech.map((item) => (
+                    tech.map((item) =>(
                         <span key={item.id}>{item.name}</span>
                     ))
                 }
-
             </div>
         </section>
     )
